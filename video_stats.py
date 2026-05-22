f009362d8af975626c6e75ef175d237c48a00ea9
kdafrose <katrosegamboa@gmail.com>
2026-05-21 20:26:28 -0600

    Added function to grab video data using video list method

--- video_stats.py
+++ video_stats.py
@@ -55,7 +55,52 @@ def get_video_id(playlistId):
 
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+# We want to extract the publishedAt, title, duration, viewCount, likeCount, and commentCount
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    #Helper function: Need to split video batched arrays to have 50 items only
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size] # will return all batches, not using return as that will end the loop right away
+    
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch) # grabs each id and separate them with a comma as a string ex: a1,b2,c3
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []): # will return 50 items or less
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None), # if not available return None
+                    "likeCount" : statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None)
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
 
 if __name__ == "__main__":
     playlistId = get_playlist_id()
-    get_video_id(playlistId)
+    video_ids_array = get_video_id(playlistId)
+
+    extract_video_data(video_ids_array)
